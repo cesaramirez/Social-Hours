462b4b7d3bb845fc27235d130e19bd174a7223b0
cesar-ramirez <ramirezcesar193@gmail.com>
2016-07-22 14:21:13 -0600

    Upload base site

---
+++ components/filter.php
@@ -0,0 +1,15 @@
+<?php
+  namespace app\components;
+
+  use Yii;
+  use yii\base\ActionFilter;
+
+  class ActionAccess extends ActionFilter
+  {
+      private function beforeAction($action)
+      {
+
+      }
+  }
+
+?>
